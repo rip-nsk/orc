d07710e9a0b213f027b6886620bae8eaea3c7efe
rip-nsk <rip.nsk@gmail.com>
2018-04-09 13:41:58 -0700

    TestTimezone.cc patch

--- c++/test/TestTimezone.cc
+++ c++/test/TestTimezone.cc
@@ -21,8 +21,20 @@
 #include "wrap/gtest-wrapper.h"
 
 #include <iostream>
-#include <unistd.h>
 #include <vector>
+
+#ifndef _MSC_VER
+#include <unistd.h>
+#else
+#define timegm _mkgmtime
+char* strptime(const char* s, const char* f, struct tm* tm) {
+  std::istringstream input(s);
+  input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
+  input >> std::get_time(tm, f);
+  if (input.fail()) return nullptr;
+  return (char*)(s + input.tellg());
+}
+#endif
 
 namespace orc {
 
@@ -123,7 +135,7 @@ namespace orc {
   const std::string& getZoneFromRule(FutureRule *rule,
                                      const std::string& date) {
     tm timeStruct;
-    if (strptime(date.c_str(), "%F %H:%M:%S", &timeStruct) == nullptr) {
+    if (strptime(date.c_str(), "%Y-%m-%d %H:%M:%S", &timeStruct) == nullptr) {
       throw TimezoneError("bad time " + date);
     }
     return rule->getVariant(timegm(&timeStruct)).name;
@@ -319,7 +331,7 @@ namespace orc {
   const std::string& getVariantFromZone(const Timezone& zone,
                                         const std::string& date) {
     tm timeStruct;
-    if (strptime(date.c_str(), "%F %H:%M:%S", &timeStruct) == nullptr) {
+    if (strptime(date.c_str(), "%Y-%m-%d %H:%M:%S", &timeStruct) == nullptr) {
       throw TimezoneError("bad time " + date);
     }
     return zone.getVariant(timegm(&timeStruct)).name;
